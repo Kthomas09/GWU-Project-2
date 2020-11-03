2e6dafe6f0fc4ac421ac6d4ce1e33a5ec6fae0d0
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-11-02 21:42:59 -0500

    created data api

--- Heroku/senators.py
+++ Heroku/senators.py
@@ -11,66 +11,44 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import pandas as pd
 import psycopg2
-# engine = create_engine("postgresql://postgres:Pepper16@localhost:5432/Candidate_db")
-# connection = engine.connect()
+from config.py import DATABASE_URL
+
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/candidates_db"
-db = SQLAlchemy(app)
-GWU = create_classes(db)
 
-
+engine = create_engine(DATABASE_URL)
+connection = engine.connect()
+cursor = connection.cursor()
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
+    message = "Welcome to my 'Home' page!"
+    return render_template("index.html", message = message)
+
+
+@app.route("/data")
+def data():
+    data = pd.read_sql("SELECT * FROM contributions_2018 WHERE state = Alabama and candidate = 'Write in';" con = connection)
+    data_dict=data.to_dict(orient="records")
+    return jsonify(data_dict)
+
+
+    # cursor.execute("select * from table_name") 
+    # data = cursor.fetchall() #data from database 
+    # return render_template("example.html", value=data)
     # print("Server received request for 'Home' page...")
     # return "Welcome! This is the analysis of the Campaign Finance Contribution In United States Senate!"
-    return render_template("index.html")
+# @app.route("/about")
+# def about():
+#     print("Server received request for 'About' page...")
+#     message = "Welcome to my 'About' page!"
+#     data = pd.read_sql(SELECT * FROM votersinfo
+#      WHERE state = "Alabama" and candidate = "Write in"; conn = connection)
 
+#     return render_template("index.html", message = message, data = data.to_html())
 
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        # name = request.form["petName"]
-        # lat = request.form["petLat"]
-        # lon = request.form["petLon"]
-
-        year =request.form["year"]
-        state= request.form["state"]
-        state_po= request.form["state_po"]
-        candidate= request.form["candidate"]
-        party = request.form["party"]
-        candidatevotes = request.form["candidatevotes"]
-        writein =request.form["writein"]
-        totalvotes =request.form["totalvotes"]
-
-	
-
-
-
-
-        gwu = GWU(year=year, state=state, state_po=state_po,candidate=candidate,party=party,
-        candidatevotes=candidatevotes,writein=writein,totalvotes=totalvotes)
-        db.session.add(GWU)
-        db.session.commit()
-        return redirect("/", code=302)
-
-        return render_template("index.html")
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    message = "Welcome to my 'About' page!"
-    data = pd.read_sql('SELECT * FROM "votersinfo" WHERE state = "Alabama" and candidate = "Write in";', connection)
-
-    return render_template("index.html", message = message, data = data.to_html())
-
-
-
-
-    
 
 if __name__ == "__main__":
     app.run(debug=True)

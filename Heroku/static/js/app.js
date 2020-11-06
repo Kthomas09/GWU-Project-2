2273b34e0fa7ff8b0bb3f8255297e5c9af2d9347
cynthiazhang88 <cynthiez96@gmail.com>
2020-11-06 18:33:09 -0500

    new

--- Heroku/static/js/app.js
+++ Heroku/static/js/app.js
@@ -58,13 +58,61 @@ Promise.all([
         
         Plotly.newPlot('Bar_Graph', data, layout, {displayModeBar: false});
 
+        var trace1 = {
+            x: [1, 2, 3, 4],
+            y: [10, 15, 13, 17],
+            mode: 'markers',
+            type: 'scatter'
+        };
 
+        var trace2 = {
+            x: [2, 3, 4, 5],
+            y: [16, 5, 11, 9],
+            mode: 'lines',
+            type: 'scatter'
+        };
+
+        var trace3 = {
+            x: [1, 2, 3, 4],
+            y: [12, 9, 15, 12],
+            mode: 'lines+markers',
+            type: 'scatter'
+        };
+
+        var data = [trace1, trace2, trace3];
+
+        Plotly.newPlot('myDiv', data);
+
+    
+
+
+    var trace1 = {
+        x: ['Alabama', 'Alaska', 'Colorado'],
+        y: [25029181, 4141799, 14766949],
+        text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+        mode: 'markers',
+        marker: {
+          color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+          size: [81.8090, 28.2400, 204.1058]
+        }
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        title: 'Bubble Chart Hover Text',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+      
+      Plotly.newPlot('BubbleChart', data, layout);
 
     //end of promise function
-    }).catch(function (error) {
-        console.log(error);
-    //end of error catching function
-    });
-
+}).catch(function (error) {
+    console.log(error);
+//end of error catching function
+});
+      
 
 
